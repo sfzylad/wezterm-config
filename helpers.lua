d6e08017de5b1daf68bb7ae41f3cfd997d226dac
Dominik Zyla <22446399+sfzylad@users.noreply.github.com>
2024-11-05 21:16:29 +0000

    Update config.
    - change the font size
    - disable ligatures
    - setup padding

--- helpers.lua
+++ helpers.lua
@@ -4,11 +4,24 @@ local module = {}
 
 local function setup_font(config)
     config.font = wezterm.font 'JetBrains Mono'
-    config.font_size = 13.0
+    config.font_size = 14.0
 end
 
 local function setup_colorscheme(config)
     config.color_scheme = 'Rosé Pine (Gogh)'
+end
+
+local function disable_ligatures(config)
+    config.harfbuzz_features = { 'calt=0' }
+end
+
+local function setup_window_padding(config)
+    config.window_padding = {
+        left = 5,
+        right = 6,
+        top = 5,
+        bottom = 5,
+    }
 end
 
 -- This sets up the deadkeys so that using vim is better.
@@ -27,6 +40,8 @@ function module.apply_to_config(config)
     setup_font(config)
     setup_colorscheme(config)
     setup_deadkey(config)
+    setup_window_padding(config)
+    disable_ligatures(config)
 end
 
 return module
