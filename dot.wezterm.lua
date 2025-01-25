897e4803b6ee6a1edb50faf135a323be13b87b2d
Dominik Zyla <22446399+sfzylad@users.noreply.github.com>
2025-01-25 16:04:33 +0000

    Current setup.

--- dot.wezterm.lua
+++ dot.wezterm.lua
@@ -1,7 +1,35 @@
 local helpers = require 'helpers'
 local wezterm = require 'wezterm'
 
+wezterm.on('toggle-light-colorscheme', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.color_scheme then
+    overrides.color_scheme = 'Rosé Pine Dawn (Gogh)'
+  else
+    overrides.color_scheme = nil
+  end
+  window:set_config_overrides(overrides)
+end)
+
+wezterm.on('toggle-dark-colorscheme', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.color_scheme then
+    overrides.color_scheme = 'Rosé Pine Dawn (Gogh)'
+    -- overrides.window_background_opacity = 0.5
+  else
+    overrides.color_scheme = nil
+  end
+  window:set_config_overrides(overrides)
+end)
+
 -- local config = {}
 local config = wezterm.config_builder()
 helpers.apply_to_config(config)
+config.keys = {
+    {
+        key = 'E',
+        mods = 'CTRL',
+        action = wezterm.action.EmitEvent 'toggle-light-colorscheme',
+    },
+}
 return config
--- helpers.lua
+++ helpers.lua
@@ -7,8 +7,12 @@ local function setup_font(config)
     config.font_size = 14.0
 end
 
-local function setup_colorscheme(config)
+local function setup_dark_colorscheme(config)
     config.color_scheme = 'Rosé Pine (Gogh)'
+end
+
+local function setup_light_colorscheme(config)
+    config.color_scheme = 'Rosé Pine Dawn (Gogh)'
 end
 
 local function disable_ligatures(config)
@@ -30,6 +34,27 @@ local function setup_deadkey(config)
     config.use_dead_keys = false
 end
 
+local function setup_quick_select(config)
+    config.leader = { key = 'Space', mods = 'CTRL|SHIFT' }
+    config.keys = {
+        {
+            key = 'a',
+            mods = 'LEADER',
+            action = wezterm.action.QuickSelect,
+        },
+    }
+end
+
+local function setup_light_colors(config)
+    config.keys = {
+        {
+            key = 'e',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.EmitEvent 'toggle-colorscheme',
+        },
+    }
+end
+
 local function setup_left_option_key(config)
     config.send_composed_key_when_left_alt_is_pressed = false
     config.send_composed_key_when_right_alt_is_pressed = true
@@ -38,10 +63,12 @@ end
 function module.apply_to_config(config)
     setup_left_option_key(config)
     setup_font(config)
-    setup_colorscheme(config)
+    setup_dark_colorscheme(config)
+    -- setup_light_colors(config)
     setup_deadkey(config)
     setup_window_padding(config)
     disable_ligatures(config)
+    setup_quick_select(config)
 end
 
 return module
